fde55418d7dd8756534306130aedcf05cb4886cc
Ryo Fuj <ryo176999@gmail.com>
2022-02-02 01:29:12 -0800

    Initial commit

---
+++ DailyCheckin/DailyCheckinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCheckinApp.swift
+//  DailyCheckin
+//
+//  Created by ryo fuj on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyCheckinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
